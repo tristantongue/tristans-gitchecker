8f94a02a966d48e63d7e00b37745dbd0365df4fa
Tristan Tongue <tristantongue@gmail.com>
2020-12-16 13:55:38 -0500

    add gitignore file

--- github_repos/settings.py
+++ github_repos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fpz$z(q%7%3y_^#3s(8j0&!na_&s0n+1d2e9d7w+h+!v6w50-+'
 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
